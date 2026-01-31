eac620ce68c6767009891119223687b1b74370a6
8ball030 <8baller@station.codes>
2026-01-31 17:55:12 +0000

    chore:linting

--- tests/test_simple_order.rs
+++ tests/test_simple_order.rs
@@ -1,11 +1,13 @@
 mod common;
 use ethereal_rust_sdk::apis::order_api::OrderControllerListBySubaccountIdParams;
-use ethereal_rust_sdk::models::{submit_order_limit_dto_data, OrderSide, OrderType};
 use ethereal_rust_sdk::models::submit_order_created_dto::Result;
+use ethereal_rust_sdk::models::{submit_order_limit_dto_data, OrderSide, OrderType};
 
 #[tokio::test]
 async fn test_simple_order() {
-    let client = common::create_test_client().await.expect("Failed to create client");
+    let client = common::create_test_client()
+        .await
+        .expect("Failed to create client");
 
     println!("Creating order...");
 
@@ -19,8 +21,8 @@ async fn test_simple_order() {
     let time_in_force = submit_order_limit_dto_data::TimeInForce::Gtd;
 
     // We have a few more options when creating an order now.
-    let mut post_only = false;
-    let mut reduce_only = false;
+    let post_only = false;
+    let reduce_only = false;
 
     let order = client
         .submit_order(
@@ -43,21 +45,30 @@ async fn test_simple_order() {
 
 #[tokio::test]
 async fn test_cancel_all_orders() {
-    let client = common::create_test_client().await.expect("Failed to create client");
+    let client = common::create_test_client()
+        .await
+        .expect("Failed to create client");
 
     let params = OrderControllerListBySubaccountIdParams {
         subaccount_id: client.subaccounts[0].id.to_string(),
         is_working: Some(true),
         ..Default::default()
     };
-    let orders = client.order().list_by_subaccount_id(params).await.expect("Failed to list orders");
+    let orders = client
+        .order()
+        .list_by_subaccount_id(params)
+        .await
+        .expect("Failed to list orders");
     if orders.data.is_empty() {
         return;
     }
-    let order_ids = orders.data.iter().map(|order| order.id.clone().to_string()).collect();
+    let order_ids = orders
+        .data
+        .iter()
+        .map(|order| order.id.clone().to_string())
+        .collect();
 
     let result = client.cancel_orders(order_ids).await;
 
     assert!(result.is_ok());
 }
-
