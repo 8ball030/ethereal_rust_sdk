0cb697aa124bccf354c807fecf89b9473b8707ca
Zarathustra <zarathustra_git@pm.me>
2026-03-30 12:48:59 +0200

    feat: add archive OpenAPI and generate code

--- build_scripts/pre_processing.sh
+++ build_scripts/pre_processing.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 echo "Collecting openapi.json..."
 curl https://api.ethereal.trade/openapi.json | jq > openapi.json
+curl https://archive.ethereal.trade/openapi.json | jq > archive_openapi.json
 
 echo "Collected RPC config files."
 curl -X 'GET' 'https://api.ethereal.trade/v1/rpc/config' \
